f4aeedbac52b2db6485d82fc6b2e68dc2c1d5fee
Thibor <Thibor@DESKTOP-6DV3OIN>
2026-02-05 16:19:21 +0100

    update

--- main.c
+++ main.c
@@ -1,5 +1,55 @@
 #include "program.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#endif
+
+const int BitTable[64] = {
+  63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2,
+  51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52,
+  26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28,
+  58, 20, 37, 17, 36, 8
+};
+
+int PopBit(U64* bb) {
+	U64 b = *bb ^ (*bb - 1);
+	unsigned int fold = (unsigned)((b & 0xffffffff) ^ (b >> 32));
+	*bb &= (*bb - 1);
+	return BitTable[(fold * 0x783a9b23) >> 26];
+}
+
+int CountBits(U64 b) {
+	int r;
+	for (r = 0; b; r++, b &= b - 1);
+	return r;
+}
+
+void PrintBitBoard(U64 bb) {
+
+	U64 shiftMe = 1ULL;
+
+	int rank = 0;
+	int file = 0;
+	int sq = 0;
+	int sq64 = 0;
+
+	printf("\n");
+	for (rank = RANK_8; rank >= RANK_1; --rank) {
+		for (file = FILE_A; file <= FILE_H; ++file) {
+			sq = FR2SQ(file, rank);	// 120 based		
+			sq64 = SQ64(sq); // 64 based
+
+			if ((shiftMe << sq64) & bb)
+				printf("X");
+			else
+				printf("-");
+
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+}
+
 U64 GeneratePosKey(const Position* pos) {
 
 	int sq = 0;
@@ -31,6 +81,65 @@ U64 GeneratePosKey(const Position* pos) {
 	finalKey ^= CastleKeys[pos->castlePerm];
 
 	return finalKey;
+}
+
+int GetTimeMs() {
+#ifdef WIN32
+	return GetTickCount64();
+#else
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	return t.tv_sec * 1000 + t.tv_usec / 1000;
+#endif
+}
+
+
+static int InputWaiting() {
+#ifndef WIN32
+	fd_set readfds;
+	struct timeval tv;
+	FD_ZERO(&readfds);
+	FD_SET(fileno(stdin), &readfds);
+	tv.tv_sec = 0; tv.tv_usec = 0;
+	select(16, &readfds, 0, 0, &tv);
+
+	return (FD_ISSET(fileno(stdin), &readfds));
+#else
+	static int init = 0, pipe;
+	static HANDLE inh;
+	DWORD dw;
+
+	if (!init) {
+		init = 1;
+		inh = GetStdHandle(STD_INPUT_HANDLE);
+		pipe = !GetConsoleMode(inh, &dw);
+		if (!pipe) {
+			SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT));
+			FlushConsoleInputBuffer(inh);
+		}
+	}
+	if (pipe) {
+		if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
+		return dw;
+	}
+	else {
+		GetNumberOfConsoleInputEvents(inh, &dw);
+		return dw <= 1 ? 0 : dw;
+	}
+#endif
+}
+
+void ReadInput(Position* pos, SearchInfo* info) {
+	int bytes;
+	char input[256] = "";
+	if (InputWaiting()) {
+		info->stop = TRUE;
+		do {
+			bytes = read(fileno(stdin), input, 256);
+		} while (bytes < 0);
+		if (strlen(input) > 0)
+			UciCommand(pos, info, input);
+	}
 }
 
 int main() {
